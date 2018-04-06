100c6ae20f317bc4ab2cb83c729fadc225fc9693
Kate Osborn <kosborn@colorado.edu>
2018-04-06 16:06:00 -0600

    finished the outline of the h file

--- math_utils.h
+++ math_utils.h
@@ -8,6 +8,8 @@ public:
   Math();
   static bool IsSquare(int num);
   int GetDigit(int num, int place);
+  bool EqualParity(int x, int y);
+  bool EqualParity(std::vector<int> nums);
 private:
 };
 
