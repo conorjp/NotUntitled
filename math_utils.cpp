097ecc5f3e769696c45a00c46f8714166a53f334
Kate Osborn <kosborn@colorado.edu>
2018-04-06 16:10:51 -0600

    changed cpp name

---
+++ math_utils.cpp
@@ -0,0 +1,19 @@
+#include "math_utils.h"
+
+class Math {
+  static bool IsSquare(int num) {
+    return false;
+  }
+
+  static int GetDigit(int num, int place) {
+    return 0;
+  }
+
+  static bool EqualParity(int x, int y) {
+    return false;
+  }
+
+  static bool EqualParity(std::vector<int> nums) {
+    return false;
+  }
+}
