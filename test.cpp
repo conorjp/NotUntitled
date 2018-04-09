c76917ac9bcf8b44a5a35ca79884b2aa0a5a6f9a
Kate Osborn <kosborn@colorado.edu>
2018-04-09 17:48:19 -0600

    added more test cases for IsSquare and GetDigit

--- test.cpp
+++ test.cpp
@@ -18,25 +18,57 @@ TEST_CASE ( "Testing function IsSquare()")
 {
     Math* math_util = new Math();
 
-  SECTION( "with square number" )
+    SECTION( "with small square number" )
     {
         REQUIRE(math_util->IsSquare(1) == true);
-
-
     }
-  SECTION( "with non-square number")
+    SECTION( "with big square number" )
+    {
+        REQUIRE(math_util->IsSquare(9801) == true);
+    }
+    SECTION( "with non-square positive number")
     {
         REQUIRE(math_util->IsSquare(3) == false);
 
-     }
+    }
+    SECTION( "with 0")
+    {
+        REQUIRE(math_util->IsSquare(0) == true);
+
+    }
+     SECTION( "with negative number ")
+    {
+        REQUIRE(math_util->IsSquare(-25) == false);
+
+    }
 }
 
 TEST_CASE("Testing function GetDigit")
 {
     Math* math_util = new Math();
-    SECTION("with valid input")
+    SECTION("with valid input and the ones place ")
     {
         REQUIRE(math_util->GetDigit(17,1) == 7);
+
+    }
+    SECTION("with valid input and the tens place ")
+    {
+        REQUIRE(math_util->GetDigit(17,10) == 1);
+
+    }
+    SECTION("with valid input and the hundreds place ")
+    {
+        REQUIRE(math_util->GetDigit(317,100) == 3);
+
+    }
+    SECTION("with valid input and the thousands place ")
+    {
+        REQUIRE(math_util->GetDigit(4317,1000) == 4);
+
+    }
+    SECTION("with valid input and the ten thousands place ")
+    {
+        REQUIRE(math_util->GetDigit(94317,10000) == 9);
 
     }
     SECTION("with invalid input")
